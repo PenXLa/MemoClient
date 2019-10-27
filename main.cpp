27706322d6663ccc14ac75889a5473b61163fd5f
XL P <921417536@qq.com>
2019-10-27 22:18:07 +0800

    建立了基本的短连接通信系统

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
-
+#include "JSONLib/json.hpp"
+#include "Net/ClientNetUtils.h"
+using nlohmann::json, std::cout;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    if (!initSocket()) std::cout << "Socket 初始化失败\n";
+    json j;
+    j["name"] = "test";
+    j["txt"] = "hw!";
+    cout << request(j);
     return 0;
 }
